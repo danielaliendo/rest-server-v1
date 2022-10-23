08af9cc96994aa13ea78d7f940616c03908d572c
Daniela Liendo <danielaliendo98@gmail.com>
2022-10-22 21:17:04 -0300

    express configuration was added and PORT env value was assigned to port constant

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
